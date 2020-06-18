679b21197a03723af8b6ee782511e42fda7bec19
shresthdubey98 <shresthdubey98@gmail.com>
2020-06-18 15:39:51 +0530

    image sending activated

--- app/src/main/java/org/docreg/app/Constants.java
+++ app/src/main/java/org/docreg/app/Constants.java
@@ -1,5 +1,7 @@
 package org.docreg.app;
 
 public class Constants {
+    static String prefix = "http://192.168.1.8:5001";
     static String authToken;
+    static String SendImageUrl = prefix + "/check-patient-exist";
 }
--- app/src/main/java/org/docreg/app/ImageCaptureScreen.java
+++ app/src/main/java/org/docreg/app/ImageCaptureScreen.java
@@ -8,19 +8,36 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.android.volley.NetworkResponse;
+import com.android.volley.NoConnectionError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ImageCaptureScreen extends AppCompatActivity {
     ImageView captureImage;
     ProgressBar progressBar;
     int CAMERA_PIC_REQUEST = 0;
+    Bitmap imageData;
     Button submitButton;
+    RequestQueue requestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,34 +47,65 @@ public class ImageCaptureScreen extends AppCompatActivity {
         submitButton = findViewById(R.id.imgsubmit);
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(takePictureIntent, CAMERA_PIC_REQUEST);
+        submitButton.setEnabled(false);
+        requestQueue = Volley.newRequestQueue(this);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final byte[] imageByte = getFileDataFromBitmap(imageData);
+                String url = Constants.SendImageUrl;
+                VolleyMultipartRequest multipartRequest = new VolleyMultipartRequest(Request.Method.POST, url, new Response.Listener<NetworkResponse>() {
+                    @Override
+                    public void onResponse(NetworkResponse response) {
+                        String resultResponse = new String(response.data);
+                        System.out.println(resultResponse);
+                        //TODO: handle response here
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //TODO: handle error here
+                    }
+                }) {
+                    @Override
+                    protected Map<String, String> getParams() {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("token", Constants.authToken);
+                        return params;
+                    }
 
+                    @Override
+                    protected Map<String, DataPart> getByteData() {
+                        Map<String, DataPart> params = new HashMap<>();
+                        params.put("image", new DataPart("image.png", imageByte , "image/png"));
+                        return params;
+                    }
+                };
+                requestQueue.add(multipartRequest);
             }
+
         });
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if (resultCode == RESULT_OK) {
-                Bitmap imageData = (Bitmap) data.getExtras().get("data");
-                imageData.CompressFormat.PNG.
+                imageData = (Bitmap) data.getExtras().get("data");
+                submitButton.setEnabled(true);
                 captureImage.setImageBitmap(imageData);
                 captureImage.setVisibility(View.VISIBLE);
                 progressBar.setVisibility(View.GONE);
+
             } else if (resultCode == RESULT_CANCELED){
                 progressBar.setVisibility(View.GONE);
             Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
             }
-//        }
         super.onActivityResult(requestCode, resultCode, data);
     }
-    public String getStringImage(Bitmap bmp) {
+    public byte[] getFileDataFromBitmap(Bitmap bmp) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         bmp.compress(Bitmap.CompressFormat.PNG, 100, baos);
-        byte[] imageBytes = baos.toByteArray();
-        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
-        return encodedImage;
+        return baos.toByteArray();
     }
 }
